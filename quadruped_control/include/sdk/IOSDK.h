754e9da0b2dadee4b7b2857fcba57a42e89c761f
Mohsen <somboles@usc.edu>
2024-07-29 14:35:43 -0700

    add sdk

---
+++ quadruped_control/include/sdk/IOSDK.h
@@ -0,0 +1,21 @@
+#ifndef IOSDK_H
+#define IOSDK_H
+
+#include "IOInterface.h"
+#include <sdk/include/unitree_legged_sdk/unitree_legged_sdk.h>
+
+using namespace UNITREE_LEGGED_SDK;
+
+class IOSDK : public IOInterface{
+public:
+IOSDK(LeggedType robot, int cmd_panel_id);
+~IOSDK(){}
+void sendRecv(const LowlevelCmd *cmd, LowlevelState *state);
+
+UDP _udp;
+Safety _control;
+LowCmd _lowCmd = {0};
+LowState _lowState = {0};
+};
+
+#endif  // IOSDK_H
