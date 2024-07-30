2466327d777b49674846756d445c71a7d9820233
mohsen <somboles@usc.edu>
2024-07-29 20:05:26 -0700

    add socket setup

--- quadruped_hardware/include/sdk/IOSDK.h
+++ quadruped_hardware/include/sdk/IOSDK.h
@@ -6,16 +6,43 @@
 
 using namespace UNITREE_LEGGED_SDK;
 
-class IOSDK : public IOInterface{
+struct DataRecv
+{
+    float quaternion[4];
+    float position[3];
+    float velocity[3];
+    float omega[3];
+    float force[3];
+};
+struct DataSend
+{
+    bool contact;
+};
+class IOSDK : public IOInterface
+{
 public:
-IOSDK(LeggedType robot, int cmd_panel_id);
-~IOSDK(){}
-void sendRecv(const LowlevelCmd *cmd, LowlevelState *state);
+    IOSDK(LeggedType robot, int cmd_panel_id);
+    ~IOSDK() {
+        loop_loco_manipulation.shutdown();
+    }
+    void sendRecv(const LowlevelCmd *cmd, LowlevelState *state);
 
-UDP _udp;
-Safety _control;
-LowCmd _lowCmd = {0};
-LowState _lowState = {0};
+    UDP _udp;
+    Safety _control;
+    LowCmd _lowCmd = {0};
+    LowState _lowState = {0};
+    void socketSendRecv();
+
+private:
+    int setupSocket(int port);
+    int serverSocket;
+    int clientSocket;
+    DataRecv _dataRecv;
+    DataSend _dataSend;
+    bool planner_running = false;
+    HighlevelCmd Highcmd;
+    Eigen::Matrix3d rotmat;
+    LoopFunc loop_loco_manipulation;
 };
 
-#endif  // IOSDK_H
+#endif // IOSDK_H
