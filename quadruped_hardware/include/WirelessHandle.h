033006a2f37cb7940294c44d3e3796009ff3209e
Mohsen <somboles@usc.edu>
2024-07-30 19:25:01 -0700

    works with Go1

--- quadruped_hardware/include/WirelessHandle.h
+++ quadruped_hardware/include/WirelessHandle.h
@@ -1,9 +1,21 @@
 #ifndef WIRELESSHANDLE_H
 #define WIRELESSHANDLE_H
 
-#include <sdk/include/unitree_legged_sdk/joystick.h>
+#ifdef GO1
+#include "sdk_3_8_0/include/unitree_legged_sdk/unitree_legged_sdk.h"
+#else
+// #include "sdk_3_3_1/include/unitree_legged_sdk/unitree_legged_sdk.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/loop.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/udp.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/comm.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/quadruped.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/safety.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/lcm.h"
+#include "sdk_3_3_1/include/unitree_legged_sdk/unitree_joystick.h"
+#endif
+#include <boost/bind.hpp>
+
 #include <interface/CmdPanel.h>
-#include <sdk/include/unitree_legged_sdk/comm.h>
 
 class WirelessHandle : public CmdPanel{
 public:
--- quadruped_hardware/src/quad_hardware_control.cpp
+++ quadruped_hardware/src/quad_hardware_control.cpp
@@ -3,7 +3,6 @@
 #include <csignal>
 #include <sched.h>
 
-
 #include <common/ControlFSMData.h>
 #include <common/ContactEstimator.h>
 
@@ -11,7 +10,7 @@
 #include <common/PositionVelocityEstimator.h>
 #include <FSM/FSM.h>
 
-#include <sdk/IOSDK.h>
+#include "IOSDK.h"
 
 using namespace UNITREE_LEGGED_SDK;
 
@@ -27,39 +26,46 @@ void setProcessScheduler()
 {
     pid_t pid = getpid();
     sched_param param;
-        param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    if(sched_setscheduler(pid, SCHED_FIFO, &param) == -1)
+    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    if (sched_setscheduler(pid, SCHED_FIFO, &param) == -1)
     {
-      std::cout << "[ERROR] function setprocessscheduler failed \n ";  
+        std::cout << "[ERROR] function setprocessscheduler failed \n ";
     }
 }
 
 int main()
 {
     setProcessScheduler();
-    
-    double dt = 0.001;
-    std::string robot_name = "go1";    
-    int cmd_panel_id = 2; // Wireless=1, keyboard=2
-    Quadruped quad(robot_name);
-    IOInterface *ioInter;
-    ioInter = new IOSDK(LeggedType::Go1, cmd_panel_id);
 
-    LegController* legController = new LegController(quad);
-    LowlevelCmd* lowCmd = new LowlevelCmd();
-    LowlevelState* lowState = new LowlevelState();
+    double dt = 0.001;
+    int cmd_panel_id = 2; // Wireless=1, keyboard=2
+    IOInterface *ioInter;
+#ifdef A1_ROBOT
+    ioInter = new IOSDK(LeggedType::A1, cmd_panel_id);
+    Quadruped quad("a1");
+#elif ALIENGO
+    ioInter = new IOSDK(LeggedType::Aliengo, cmd_panel_id);
+    Quadruped quad("aliengo");
+#elif GO1
+    ioInter = new IOSDK(LeggedType::Go1, cmd_panel_id);
+    Quadruped quad("go1");
+#endif
+
+    LegController *legController = new LegController(quad);
+    LowlevelCmd *lowCmd = new LowlevelCmd();
+    LowlevelState *lowState = new LowlevelState();
     StateEstimate stateEstimate;
-    StateEstimatorContainer* stateEstimator = new StateEstimatorContainer(lowState,
+    StateEstimatorContainer *stateEstimator = new StateEstimatorContainer(lowState,
                                                                           legController->data,
                                                                           &stateEstimate);
-                                                                        
+
     stateEstimator->addEstimator<ContactEstimator>();
     stateEstimator->addEstimator<VectorNavOrientationEstimator>();
     stateEstimator->addEstimator<LinearKFPositionVelocityEstimator>();
 
-    DesiredStateCommand* desiredStateCommand = new DesiredStateCommand(&stateEstimate, dt);
+    DesiredStateCommand *desiredStateCommand = new DesiredStateCommand(&stateEstimate, dt);
 
-    ControlFSMData* _controlData = new ControlFSMData;
+    ControlFSMData *_controlData = new ControlFSMData;
     _controlData->_quadruped = &quad;
     _controlData->_stateEstimator = stateEstimator;
     _controlData->_legController = legController;
@@ -68,23 +74,21 @@ int main()
     _controlData->_lowCmd = lowCmd;
     _controlData->_lowState = lowState;
 
-    FSM* _FSMController = new FSM(_controlData);
+    FSM *_FSMController = new FSM(_controlData);
 
     LoopFunc loop_control("control_loop", dt, boost::bind(&FSM::run, _FSMController));
-    LoopFunc loop_udpSend("udp_send",     dt, 3, boost::bind(&ControlFSMData::sendRecv, _controlData));
-
+    LoopFunc loop_udpSend("udp_send", dt, 3, boost::bind(&ControlFSMData::sendRecv, _controlData));
 
     loop_udpSend.start();
     loop_control.start();
 
-
-    while(1){
+    while (1)
+    {
         +sleep(10);
     }
-    
+
     delete _FSMController;
     delete _controlData;
 
     return 0;
-
 }
