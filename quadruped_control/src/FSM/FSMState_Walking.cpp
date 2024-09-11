44deb5ffeb64367532a691a3a5a055f943de20f0
mohsen <somboles@usc.edu>
2024-09-10 19:23:22 -0700

    update

--- quadruped_control/src/FSM/FSMState_Walking.cpp
+++ quadruped_control/src/FSM/FSMState_Walking.cpp
@@ -42,17 +42,17 @@ void FSMState_Walking::run()
 
     if (counter > 2000 && counter < 6000)
     {
-        _data->_quadruped->COM_height = 0.35 - ((counter - 2000.0) / 4000.0) * 0.1;
+        _data->_quadruped->COM_height = 0.35 - ((counter - 2000.0) / 4000.0) * 0.13;
     }
 
     else if (counter > 6000 && counter < 10000)
     {
-        _data->_quadruped->COM_height = 0.25;
+        _data->_quadruped->COM_height = 0.22;
     }
 
     else if (counter > 10000 && counter < 16000)
     {
-        _data->_quadruped->COM_height = 0.25 + ((counter - 10000.0) / 4000.0) * 0.1;
+        _data->_quadruped->COM_height = 0.22 + ((counter - 10000.0) / 4000.0) * 0.13;
     }
 
     else{
