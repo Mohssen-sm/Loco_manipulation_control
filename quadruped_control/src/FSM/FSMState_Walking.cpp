d2dedbadbf669df1eeed1a9dd0d4fd4fb605a283
mohsen <somboles@usc.edu>
2024-09-10 18:37:39 -0700

    update the height and velocity

--- quadruped_control/src/FSM/FSMState_Walking.cpp
+++ quadruped_control/src/FSM/FSMState_Walking.cpp
@@ -1,12 +1,13 @@
 #include <FSM/FSMState_Walking.h>
 
 FSMState_Walking::FSMState_Walking(ControlFSMData *data)
-                 :FSMState(data, FSMStateName::WALKING, "walking"),
-                 Cmpc(0.001, 10, data->_quadruped){}
+    : FSMState(data, FSMStateName::WALKING, "walking"),
+      Cmpc(0.001, 10, data->_quadruped) {}
 
-template<typename T0, typename T1, typename T2>
-T1 invNormalize(const T0 value, const T1 min, const T2 max, const double minLim = -1, const double maxLim = 1){
-	return (value-minLim)*(max-min)/(maxLim-minLim) + min;
+template <typename T0, typename T1, typename T2>
+T1 invNormalize(const T0 value, const T1 min, const T2 max, const double minLim = -1, const double maxLim = 1)
+{
+    return (value - minLim) * (max - min) / (maxLim - minLim) + min;
 }
 
 void FSMState_Walking::enter()
@@ -17,55 +18,81 @@ void FSMState_Walking::enter()
     //  _data->_interface->zeroCmdPanel();
     counter = 0;
     Cmpc.firstRun = true;
-    _data->_stateEstimator->run(); 
+    _data->_stateEstimator->run();
     // _data->_legController->zeroCommand();
 }
 
 void FSMState_Walking::run()
 {
     _data->_legController->updateData(_data->_lowState);
-    _data->_stateEstimator->run(); 
+    _data->_stateEstimator->run();
     _userValue = _data->_lowState->userValue;
     // adjusthte velocity range based on the gait cycle time
     // v_des_body[0] = (double)invNormalize(_userValue.ly, -3.0, 3.0); // change this velocity limit for different robots
-    v_des_body[0] = (double)invNormalize(_userValue.ly, -1.0, 1.0); // Aliengo 
+    v_des_body[0] = (double)invNormalize(_userValue.ly, -1.0, 1.0); // Aliengo
+    if (abs(v_des_body[0]) < 0.01)
+    {
+        v_des_body[0] = 0.15;
+    }
+
     v_des_body[1] = (double)invNormalize(_userValue.rx, 0.5, -0.5);
     turn_rate = (double)invNormalize(_userValue.lx, 2.0, -2.0);
 
     _data->_desiredStateCommand->setStateCommands(roll, pitch, v_des_body, turn_rate);
-    
+
+    if (counter > 2000 && counter < 3500)
+    {
+        _data->_quadruped->COM_height = 0.3 - ((counter - 2000) / 1500) * 0.1;
+    }
+
+    else if (counter > 3500 && counter < 5500)
+    {
+        _data->_quadruped->COM_height = 0.2;
+    }
+
+    else if (counter > 5500 && counter < 7000)
+    {
+        _data->_quadruped->COM_height = 0.2 + ((counter - 2000) / 1500) * 0.1;
+    }
+
     Cmpc.climb = true;
-    Cmpc.setGaitNum(2); 
+    Cmpc.setGaitNum(2);
     Cmpc.run(*_data);
 
-    _data->_legController->updateCommand(_data->_lowCmd);  
+    _data->_legController->updateCommand(_data->_lowCmd);
+    counter++;
 }
 
 void FSMState_Walking::exit()
-{      
-    counter = 0; 
+{
+    counter = 0;
     _data->_interface->cmdPanel->setCmdNone();
 }
 
 FSMStateName FSMState_Walking::checkTransition()
 {
-    if(_lowState->userCmd == UserCommand::L1_X || walking2QP){
-        if(Cmpc.phase > 0.96){
+    if (_lowState->userCmd == UserCommand::L1_X || walking2QP)
+    {
+        if (Cmpc.phase > 0.96)
+        {
             std::cout << "transition from walk to QP stand" << std::endl;
             walking2QP = false;
             return FSMStateName::QPSTAND;
         }
-        else{
+        else
+        {
 
             walking2QP = true;
             return FSMStateName::WALKING;
         }
     }
-    else if(_lowState->userCmd == UserCommand::L2_B){
+    else if (_lowState->userCmd == UserCommand::L2_B)
+    {
         std::cout << "transition from walking to passive" << std::endl;
         return FSMStateName::PASSIVE;
     }
-    else{
+    else
+    {
         return FSMStateName::WALKING;
     }
 }
